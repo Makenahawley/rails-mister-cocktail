50180e1e2539200bdaeb3987a05e74b0771f0f23
Makena Hawley <makena.hawley@gmail.com>
2018-11-10 16:19:49 +0100

    took me 4everrrr

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "Deleting all ingredients..."
+Ingredient.destroy_all
+
+puts "Fetching ingredients from the API..."
+ingredient_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(ingredient_url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+puts "Creating ingredients..."
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+puts "Deleting all cocktails..."
+Cocktail.destroy_all
+
+puts "Creating 3 cocktails..."
+c1 = Cocktail.create(name: 'Margarita')
+c2 = Cocktail.create(name: 'Manhattan')
+c3 = Cocktail.create(name: 'French Martini')
+
